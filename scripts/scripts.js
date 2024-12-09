3a8c8092abe14fd13f36342f99f3fdd41c5d27a9
ripquan <palmaj@bu.edu>
2024-12-08 21:31:19 -0500

    progress

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,16 +4,54 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+const spotMenu = document.getElementById('spot-menu');
+const header = document.getElementById('header');
+let spotName = document.getElementById('spot-name');
+let description = document.getElementById('description');
+let spotPic = document.getElementById('spot-pic');
+let spotType = document.getElementById('spot-type');
+let rating = document.getElementById('rating');
+let bustFactor = document.getElementById('bust-factor');
+let uploadBtn = document.getElementById('upload');
 
-const addSpot = document.getElementById('new-spot');
-addSpot.addEventListener('click', openNav);
 
-function openNav() {
-    document.getElementById("spot-menu").style.height = "100%";
-    console.log('opened')
-  }
+
+map.on("dblclick", function(e){
+   var marker = new L.Marker([e.latlng.lat, e.latlng.lng]).addTo(map);
+   spotMenu.classList.toggle("open");
+   header.classList.toggle("close");
+   
+   function pinInfo(){
+    let info = {};
+    info.spotName = spotName.value;
+    info.description = description.value;
+    info.spotPic = spotPic.value;
+    info.spotType = spotType.value;
+    info.rating = rating.value;
+    info.bustFactor = bustFactor.value;
+    marker.bindPopup(`Spot Name: ${info.spotName}\nDescription: ${info.description}\n${info.spotPic}\nSpot Type: ${info.spotType}\nRating: ${info.rating}  Bust Factor: ${info.bustFactor}`).openPopup();
+    spotMenu.classList.toggle("open");
+    header.classList.toggle("close");
+   }
   
-  /* Close */
-  function closeNav() {
-    document.getElementById("spot-menu").style.height = "0%";
-  }
+   uploadBtn.addEventListener('click', pinInfo);
+   
+
+})
+
+// function pinInfo(){
+// 	let info = {};
+// 	info.spotName = spotName.value;
+// 	info.description = description.value;
+// 	info.spotPic = spotPic.value;
+// 	info.spotType = spotType.value;
+//   info.rating = rating.value;
+//   info.bustFactor = bustFactor.value;
+// 	console.log(info);
+// }
+
+// uploadBtn.addEventListener('click', pinInfo);
+
+// function addPin() {
+
+// }
